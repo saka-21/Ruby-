6e23dbf2c9045e08cc3b8d5760e6728805dd6db1
saka-21 <kasaikasai572@gmail.com>
2020-11-19 21:59:49 +0900

    first commit

---
+++ Greeting.rb
@@ -0,0 +1,44 @@
+# # putsは改行される。printは改行されない
+# puts 'Good Morning'
+
+# puts 'Good Afternoon'
+
+# puts 'Good Evening'
+
+# puts 'Good Evening'
+
+# num = 1
+# puts num
+# p num # putsは「p」と省略できる
+
+# num_2 = 2
+# num_4 = 4
+
+# p num_4
+
+# NUM = 100
+# num = 50
+
+# p NUM
+
+# p 10.div(10)
+# p 10.remainder(4)
+
+p 10.class
+p 10.0.class
+puts 'Good Morning'.index('d')
+puts 'Good Morning'.length
+puts 'Good Morning'.class
+
+p (10>1)
+p (10>1).class
+
+a = Array.new(3)
+a[0] = ("sato")
+a[1] = ("sato")
+a[2] = ("sato")
+a[1] = ("tanaka")
+p a
+
+b = ["sato", "sato", "sato"]
+print b
